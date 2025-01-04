378a567f22c93621dbce405860455277ab014566
Natalie Rolf <nrolf99@gmail.com>
2025-01-03 22:35:41 -0600

    fix

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, input, isStandalone } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ListItemViewModel } from '../models/listItemViewModel';
 
 @Component({
