c4a297c02d90b53977538bcb0a89051c35febeb1
Natalie Rolf <nrolf99@gmail.com>
2025-01-04 19:01:22 -0600

    fix date, create menu-modal component and files, add comment to test data in home page, modal open on click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-
+import {ModalController} from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { MenuModalComponent } from '../menu-modal/menu-modal.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  providers: [
+    ModalController
+  ]
 })
 export class HeaderComponent  implements OnInit {
 
   dateString: string | null = "";
+  newItem: string | null = "";
 
-  constructor(private datePipe: DatePipe) { 
-    this.dateString = this.datePipe.transform(new Date(), 'EEEE, MMMM yyyy');
+  constructor(private datePipe: DatePipe, private modalController: ModalController) { 
+    this.dateString = this.datePipe.transform(new Date(), 'EEEE MMMM dd, yyyy');
 
   }
 
 
   ngOnInit() {}
+
+
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: MenuModalComponent,
+    });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm') {
+      this.newItem = `New to do item: ${data}!`;
+    }
+  }
 
 }
---
+++ src/app/menu-modal/menu-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-modal',
+  templateUrl: './menu-modal.component.html',
+  styleUrls: ['./menu-modal.component.scss'],
+})
+export class MenuModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
